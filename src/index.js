c937e10e5ab052c46f3ac0b7869dc149d06f8680
Elena Valero <elenavaleroesco@gmail.com>
2024-03-16 11:23:57 +0100

    fix errors

--- src/index.js
+++ src/index.js
@@ -131,76 +131,58 @@ server.get('/projectCard/:id', (req, res) => {
 
 //definir EndPoint
 
-// Crear proyectos
-app.post('/api/projectCard', async (req, res) => {
+// // Crear proyectos
+// app.post('/api/projectCard', async (req, res) => {
 
-  // Datos vienen req.body
+//   // Datos vienen req.body
 
-  // 1. Conectar a la bbdd
+//   // 1. Conectar a la bbdd
 
-  const conn = await getConnection();
+//   const conn = await getConnection();
 
-  // 2. Insertar los datos de la autora  Authors
- const insertAuthor = `
-  INSERT authors (autor, job, image)
-    VALUES (?, ?, ?)`
+//   // 2. Insertar los datos de la autora  Authors
+//  const insertAuthor = `
+//   INSERT authors (autor, job, image)
+//     VALUES (?, ?, ?)`
 
-const [resultsInsertAuthor] = await conn.execute(insertAuthor, [req.body.autor, req.body.job, req.body.image]); 
+// const [resultsInsertAuthor] = await conn.execute(insertAuthor, [req.body.autor, req.body.job, req.body.image]); 
 
-  // 3. Recupero el id de Authors
+//   // 3. Recupero el id de Authors
 
-  const fkAuthor = resultsInsertAuthor.insertId;
+//   const fkAuthor = resultsInsertAuthor.insertId;
 
-  // 4. Insertar el proyecto Projects(fkAuthors)
+//   // 4. Insertar el proyecto Projects(fkAuthors)
   
-  const insertProject = `
-  INSERT projects (name, slogan, repo, demo, technologies, \`desc\`, photo, fkAutor) 
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
+//   const insertProject = `
+//   INSERT projects (name, slogan, repo, demo, technologies, \`desc\`, photo, fkAutor) 
+//     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);`;
 
-    const [resultsInsertProject] = await conn.execute(
-      insertProject,
-      [req.body.name, req.body.sloga, req.body.repo, req.body.demo, req.body.technologies, req.body.desc, req.body.photo, fkAuthor]
-    )
-
-
-  // 5. Recupero el id de Projects
-
-const idProject = resultsInsertProject.insertId
-
-  // 6. Cierro al conexion
-
-  conn.end();
-
-  // 7. Devuelvo el json
-res.json({ 
-  success: true,
-  cardURL: `https://localhost:${port}/projectCard/${idProject}`
-});
-
-});
+//     const [resultsInsertProject] = await conn.execute(
+//       insertProject,
+//       [req.body.name, req.body.sloga, req.body.repo, req.body.demo, req.body.technologies, req.body.desc, req.body.photo, fkAuthor]
+//     )
 
 
-// API listar proyectos
-app.get('/api/proyectCard', (req, res) => {
+//   // 5. Recupero el id de Projects
 
-  // 1. Conectar a la bbdd
+// const idProject = resultsInsertProject.insertId
 
-  
-  // 2. Lanzar un SELECT para recuperar todos los proy de la bbdd
-  
-  
+//   // 6. Cierro al conexion
 
-  // 3. Cierro la conexión
+//   conn.end();
 
-  
-  // 4. Devuelvo un json con los resultados.
+//   // 7. Devuelvo el json
+// res.json({ 
+//   success: true,
+//   cardURL: `https://localhost:${port}/projectCard/${idProject}`
+// });
 
-});
+// });
 
 
 
 // Mostrar el detalle de un proyecto (serv. dinámicos)
-app.get('/projectCard/:id', async (req, res) => {
+server.get('/projectCard/:id', async (req, res) => {
 
   // Recibo el id del proyecto en un URL param
 
